ea841cfc5725cf36912eacdd0e9ffb7a5c9dd44c
Muhammad Tayyab <Bcsf17a512@pucit.edu.pk>
2020-12-19 22:49:20 +0500

    navbar of drawer added

--- app/src/main/java/com/example/drawerapp/MainActivity.java
+++ app/src/main/java/com/example/drawerapp/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.drawerapp;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity<toolbar> extends AppCompatActivity {
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView=findViewById(R.id.nav_view);
+        drawerLayout=findViewById(R.id.drawer);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
     }
+
+
 }
